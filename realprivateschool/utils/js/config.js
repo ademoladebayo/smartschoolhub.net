e2883729f5b10b8f67934c080bd9f054097c7322
ademoladebayo <ademoladebayo54@gmail.com>
2023-03-27 23:56:58 +0100

    Payment breakdown and optional fee approval V1

--- realprivateschool/utils/js/config.js
+++ realprivateschool/utils/js/config.js
@@ -1,5 +1,5 @@
 export default class Config {
-  APP_MODE = "LIVE";
+  APP_MODE = "DEV";
   SCHOOL_NAME = "realprivateschool"; //realprivateschool
   url = {
     ip: "",
@@ -12,7 +12,7 @@ export default class Config {
       this.url.ip = "http://127.0.0.1:8000";
       this.url.domain =
         "http://localhost/smartschoolhub.net/" + this.SCHOOL_NAME;
-    } else if (this.APP_MODE == "DEV") {
+    } else if (this.APP_MODE == "LIVE") {
       // LIVE IP
       this.url.ip = "https://smartschoolhub.net/backend/" + this.SCHOOL_NAME;
       this.url.domain = "https://" + this.SCHOOL_NAME + ".smartschoolhub.net";
