5ffea05df724d12c00329a400a98505077025840
ademoladebayo <ademoladebayo54@gmail.com>
2023-07-29 17:43:50 +0100

    Dashboard fix v1.1

--- mss/bursary/js/bursary.js
+++ mss/bursary/js/bursary.js
@@ -1933,6 +1933,63 @@ async function getDashboardInfo() {
   // }
 }
 
+ function getDashboardInfo2() {
+   openSpinnerModal("Statistics for "+ localStorage["current_session"] +" - "+ localStorage["current_term"])
+   fetch(ip + "/api/bursary/dashboard-information", {
+     method: "POST",
+     headers: {
+       Accept: "application/json",
+       "Content-type": "application/json",
+       Authorization: "Bearer " + localStorage["token"],
+     },
+     body: JSON.stringify({
+       session: localStorage["current_session"],
+       term: localStorage["current_term"],
+     }),
+   })
+     .then(function (res) {
+       console.log(res.status);
+       if (res.status == 401) {
+         openAuthenticationModal();
+       }
+       return res.json();
+     })
+ 
+     .then((data) => {
+       removeSpinnerModal();
+       //DASHBOARD CHART DATA
+       dashboardChart(JSON.stringify(data.chart_data));
+ 
+       document.getElementById("student_no").innerHTML = `<span class="counter"
+       data-num="${data.student_no}">${data.student_no}</span>`;
+ 
+       document.getElementById("total_manual_payment").innerHTML = formatNumber(
+         parseInt(data.total_manual_payment)
+       );
+ 
+       document.getElementById("total_arrears").innerHTML = formatNumber(
+         parseInt(data.total_arrears)
+       );
+ 
+       document.getElementById("total").innerHTML =
+         "₦" +
+         formatNumber(
+           parseInt(data.total_arrears) + parseInt(data.total_manual_payment)
+         );
+ 
+       document.getElementById("total_expense").innerHTML = formatNumber(
+         parseInt(data.total_expense)
+       );
+ 
+       document.getElementById("total_debt").innerHTML = formatNumber(data.total_debt);
+     })
+     .catch((err) => console.log(err));
+ 
+   // if(document.getElementById("total").innerHTML == "---"){
+ 
+   // }
+ }
+
 function dashboardChart(chart_data) {
   chart_data = JSON.parse(chart_data);
   current_term = localStorage['current_term'].toLowerCase().replace(' ',"_");
@@ -2330,7 +2387,7 @@ function useCustomSessionTerm(session_term) {
   console.log(session_term);
   localStorage.setItem("current_session", session_term.split("-")[0]);
   localStorage.setItem("current_term", session_term.split("-")[1]);
-  getDashboardInfo();
+  getDashboardInfo2();
 }
 
 //CHECK PORTAL SUBSCRIPTION
