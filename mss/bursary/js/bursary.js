9177701139b396cb553a730376aee496a207af75
ademoladebayo <ademoladebayo54@gmail.com>
2023-08-24 13:01:47 +0100

    Payment history fix v1.1

--- mss/bursary/js/bursary.js
+++ mss/bursary/js/bursary.js
@@ -615,7 +615,11 @@ function getFee(student_id, session, term, student_class) {
                    ? `OPTIONAL (Approved)`
                    : fee.type
                }</td>
-               <td>${ data.class_name}</td>
+               <td>${
+                 fee.class == localStorage["PD_STUDENT_CLASS"] || data.class_name == localStorage["PD_STUDENT_CLASS"]
+                   ? localStorage["PD_STUDENT_CLASSNAME"]
+                   : fee.class
+               }</td>
               <td>₦${formatNumber(fee.amount)}</td>
           </tr>
           `;
