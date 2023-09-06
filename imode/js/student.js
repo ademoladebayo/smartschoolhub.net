06b7fffeffcaad1fe35db012c323726ae938ad72
Ademola Adebayo <ademola.adebayo@etranzact.com>
2023-09-06 11:23:21 +0100

    Deleted unwanted files v1

--- imode/js/student.js
+++
@@ -1,9 +0,0 @@
-function loadExam() {
-  var d = new Date();
-  console.log("work");
-  window.parent.document.getElementById("iframe").src = "";
-  var iframe = (window.parent.document.getElementById("iframe").src =
-    "../exam.html?ver=" + d.getTime());
-
-  //   iframe.src = "../exam.html";
-}
