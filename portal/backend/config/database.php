7615fcc85e07ea23619a0de7109da31ce6b397c1
Ademola Adebayo <ademola.adebayo@etranzact.com>
2023-10-04 21:14:27 +0100

    Onboarded new school

--- portal/backend/config/database.php
+++ portal/backend/config/database.php
@@ -173,6 +173,26 @@ return [
             ]) : [],
         ],
 
+        'it' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'smartsc1_INVINCIBLETUTORS'),
+            'username' => env('DB_USERNAME', 'smartsc1_general_user'),
+            'password' => env('DB_PASSWORD', 'general_user'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'ds' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
