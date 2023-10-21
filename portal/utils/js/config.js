cdf23d84cdff58fb93b3e2975286476b4424322a
Ademola Adebayo <ademola.adebayo@etranzact.com>
2023-10-21 17:34:28 +0100

    Domain route update

--- portal/utils/js/config.js
+++ portal/utils/js/config.js
@@ -7,7 +7,7 @@ export default class Config {
   };
 
   constructor() {
-    if (this.APP_MODE == "DEV") { 
+    if (this.APP_MODE == "DEV") {
       // DEVELOPMENT IP
       this.url.ip = "http://127.0.0.1:8000";
       this.url.domain =
@@ -15,11 +15,12 @@ export default class Config {
     } else if (this.APP_MODE == "LIVE") {
       // LIVE IP
       this.url.ip = "https://smartschoolhub.net/backend/" + this.SCHOOL_NAME;
-      this.url.domain = "https://" + this.SCHOOL_NAME + ".smartschoolhub.net";
+      // this.url.domain = "https://" + this.SCHOOL_NAME + ".smartschoolhub.net";
+      this.url.domain = "https://smartschoolhub.net/" + this.SCHOOL_NAME + "/";
     } else {
-       // TEST-LIVE IP
-       this.url.ip = "https://smartschoolhub.net/backend/" + this.SCHOOL_NAME;
-       this.url.domain =
+      // TEST-LIVE IP
+      this.url.ip = "https://smartschoolhub.net/backend/" + this.SCHOOL_NAME;
+      this.url.domain =
         "http://localhost/smartschoolhub.net/portal";// + this.SCHOOL_NAME;
     }
   }
