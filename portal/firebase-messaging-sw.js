e72bcdc375939f48d617651ea971e44f4810a955
ademola.adebayo <adeademola.adebayo@etranzact.com>
2025-05-17 20:59:52 +0100

    Revive fcm /portal --

---
+++ portal/firebase-messaging-sw.js
@@ -0,0 +1,44 @@
+// /*
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js"
+);
+
+/*
+Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+* New configuration for app@pulseservice.com
+*/
+firebase.initializeApp({
+  apiKey: "AIzaSyCLhWTc_4e5rGJeXV8qGCWZdZLTP0YrjCA",
+  authDomain: "dextroux-technologies.firebaseapp.com",
+  projectId: "dextroux-technologies",
+  storageBucket: "dextroux-technologies.appspot.com",
+  messagingSenderId: "1099192792266",
+  appId: "1:1099192792266:web:2da00b0f913d84ec4ef033",
+  measurementId: "G-QWNY4DPSNH",
+});
+
+/*
+Retrieve an instance of Firebase Messaging so that it can handle background messages.
+*/
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log(
+    "[firebase-sw.js] Received background message ",
+    payload
+  );
+  /* Customize notification here */
+ const notificationTitle = payload.data.title;
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: "https://portal.smartschoolhub.net/icons/120.png",
+     sound: "https://portal.smartschoolhub.net/asset/sound/verified.mp3",
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
