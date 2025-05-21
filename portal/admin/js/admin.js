31830cfdf6e2bee157273e681f69bb9dd311c4c4
ademola.adebayo <adeademola.adebayo@etranzact.com>
2025-05-21 21:48:46 +0100

    Restruture Folder v1.3.5

--- portal/admin/js/admin.js
+++ portal/admin/js/admin.js
@@ -165,13 +165,13 @@ async function signIn() {
               localStorage.setItem('register_device', '1');
               errortoast("Device not registered.");
 
-              alert("Flagged to register Device token: " + deviceToken + " --- " + data.data.device_token + " ---" + needsRegistration);
+              // alert("Flagged to register Device token: " + deviceToken + " --- " + data.data.device_token + " ---" + needsRegistration);
             }
           }
 
           // Send token to server if registered
           if (deviceToken && needsRegistration) {
-            alert("About to register Device token: " + deviceToken + " --- " + data.data.device_token + " ---" + needsRegistration);
+            // alert("About to register Device token: " + deviceToken + " --- " + data.data.device_token + " ---" + needsRegistration);
             try {
               const userType = "ADMIN";
               await sendTokenToServer(deviceToken, userType, data.data.id);
