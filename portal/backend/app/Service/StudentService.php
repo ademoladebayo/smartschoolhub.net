54fc65da74535d9a1da74e2c6aba4ba5f7741d90
ademola.adebayo <adeademola.adebayo@etranzact.com>
2025-06-27 21:03:59 +0100

    TRANSCRIPT REMARK V1.7

--- portal/backend/app/Service/StudentService.php
+++ portal/backend/app/Service/StudentService.php
@@ -525,9 +525,7 @@ class StudentService
 
             $third_term = SubjectRegistrationModel::select(DB::raw('(first_ca + second_ca + note_assignment + cbt + project + examination) as total'))->where(["subject_id" => $data->subject_id, "student_id" => $request->student_id])->where("session", $request->session)->where("term", "THIRD TERM")->first();
 
-            Log::debug("FIRST TERM : " . $first_term);
-            Log::debug("SECOND TERM : " . $second_term);
-            Log::debug("THIRD TERM : " . $third_term);
+           
 
             $first_term = $first_term ? intval($first_term->total) : '-';
             $second_term = $second_term ? intval($second_term->total) : '-';
@@ -559,6 +557,11 @@ class StudentService
                 } else {
                     $third_termm = intval($third_term);
                 }
+
+                 Log::debug("FIRST TERM : " . $first_term);
+            Log::debug("SECOND TERM : " . $second_term);
+            Log::debug("THIRD TERM : " . $third_term);
+            Log::debug("term_count : " . $term_count);
 
                 // MEAN SCORE FOR THIRD TERM
                 if ($term_count == 0) {
