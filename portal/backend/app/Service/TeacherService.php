f29aeeac895a51fdd4689ccbd792328165c5c607
ademola.adebayo <adeademola.adebayo@etranzact.com>
2025-07-05 16:31:38 +0100

    CUSTOMIZED RESULT HEADER -- v2.2

--- portal/backend/app/Service/TeacherService.php
+++ portal/backend/app/Service/TeacherService.php
@@ -330,7 +330,7 @@ class TeacherService
         $max = SubjectRegistrationModel::select(DB::raw('max(total) as max'))->where("subject_id", $request->subject_id)->where("session", $request->session)->where("term", $request->term)->get()[0]->max;
 
         // GET RESULT FORMAT
-        $class_sector = SubjectModel::where('id', $request->subject_id)->get();//->class->class_sector;
+        $class_sector = SubjectModel::get();//->class->class_sector;
 
         Log::debug($request->subject_id);
         Log::debug($class_sector);
