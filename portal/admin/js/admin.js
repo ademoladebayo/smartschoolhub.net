1662b8a60fb4468faced105263c5bb10a488226f
ademola.adebayo <adeademola.adebayo@etranzact.com>
2025-07-11 19:28:53 +0100

    BROADSHEET -- v3.9

--- portal/admin/js/admin.js
+++ portal/admin/js/admin.js
@@ -625,11 +625,11 @@ function reloadEditFrame() {
   iframe.src = temp;
 }
 
-function reloadFrame(frame) {
+function reloadFrame(frame, new_page = null) {
   var iframe = document.getElementById(frame);
   temp = iframe.src;
   iframe.src = "";
-  iframe.src = temp;
+  iframe.src = new_page ? new_page : temp;
 }
 
 // TEACHER
@@ -2563,7 +2563,6 @@ function uploadCommentAndRating(type, value, rating_type) {
 
 // BROADSHEET RESULT
 function getBroadsheet() {
-  openSpinnerModal("Broadsheet is being generated, please wait...");
 
   // SCHOOL LOGO URL
   const school_logo_url = domain + "/backend/storage/app/public/fileupload/" + localStorage["school"] + "/school_logo.png";
@@ -2579,10 +2578,10 @@ function getBroadsheet() {
   };
 
   if (!params.class || !params.term || !params.session) {
-    removeSpinnerModal();
-    document.getElementById('broadsheet').innerHTML = `<h3 class="text-center">Please select a class, term and session to generate broadsheet.</h3>`;
-    return;
+    return 0;
   }
+
+  openSpinnerModal("Broadsheet is being generated, please wait...");
 
   fetch(ip + `/api/admin/broadsheet?` + new URLSearchParams(params), {
     method: "GET",
@@ -7659,106 +7658,112 @@ async function sendTokenToServer(deviceToken, user_type, id) {
     .catch((err) => console.log(err));
 }
 
+
 // RE - AUTHENTICATION MODAL
 function openAuthenticationModal() {
-  var modal = `<div class="modal fade" id = "authenticationModal" tabindex = "-1" role = "dialog"
-aria - labelledby="endModalTitle" aria - hidden="true" data - backdrop="static" data - keyboard="false">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+  modal = `<div class="modal fade" id="authenticationModal" tabindex="-1" role="dialog"
+aria-labelledby="endModalTitle" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h4 style="font-family: Poppins; font-weight: bold;"
-          class="modal-title col-12 text-center" id="authenticationModalTitle">
-          <b>Session Timeout !</b>
-        </h4>
+        <div class="modal-header">
+            <h4 style="font-family: Poppins; font-weight: bold;"
+                class="modal-title col-12 text-center" id="authenticationModalTitle">
+                <b>Session Timeout !</b>
+            </h4>
 
-      </div>
-      <div class="modal-body text-center">
-        <div class="row">
-          <div class="col-lg-12 img-box">
-            <img src="../asset/images/login-banner.png" alt="">
-          </div>
-          <div class="col-lg-12 no-padding">
-            <div class="login-box">
-              <link rel="stylesheet" type="text/css" href="../asset/css/style.css" />
-              <link href="../assets/css/lib/toastr/toastr.min.css" rel="stylesheet">
-                <link href="../assets/css/lib/sweetalert/sweetalert.css" rel="stylesheet">
-                  <div style="display: flex;
+        </div>
+        <div class="modal-body text-center">
+            <div class="row">
+                <div class="col-lg-12 img-box">
+                    <img src="../asset/images/login-banner.png" alt="">
+                </div>
+                <div class="col-lg-12 no-padding">
+                    <div class="login-box">
+                        <link rel="stylesheet" type="text/css" href="../asset/css/style.css" />
+                        <link href="../assets/css/lib/toastr/toastr.min.css" rel="stylesheet">
+                        <link href="../assets/css/lib/sweetalert/sweetalert.css" rel="stylesheet">
+                        <div style="display: flex;
                         justify-content: center;" class="row">
 
-                    <b>
-                      <h3 style="font-weight: bold; font-family: Rowdies; color:#051f3e;">
-                        <i style="color: #051f3e;"
-                          class="fas fa-graduation-cap fa-xs"></i>
-                        SMARTSCHOOLHUB.net
-                      </h3>
-                    </b>
+                            <b>
+                                <h3 style="font-weight: bold; font-family: Rowdies; color:#051f3e;">
+                                    <i style="color: #051f3e;"
+                                        class="fas fa-graduation-cap fa-xs"></i>
+                                    SMARTSCHOOLHUB.net
+                                </h3>
+                            </b>
 
-                  </div>
-                  <br>
-
-                    <h5 style="color: #ff9d01; font-family: Poppins; font-weight: bold;">Hi
-                      ${localStorage["username"]},</script> please
-                    signin
-                    to continue
-                  </h5>
-                  <form autocomplete="off">
-                    <label for=""><i class="fas fa-unlock-alt"></i> Password</label>
-                    <div class="login-row row no-margin">
-
-                      <input id="password" type="password" autocomplete="new-password"
-                        class="form-control form-control-sm">
-                        <br>
-                          <small id="togglePass" style="cursor:pointer; font-style:bold">Show password</small>
                         </div>
-                      </form>
-                      <br>
-                        <a style="float: right; color: red;" href="./index.html">Log out</a>
+                        <br>
+
+                        <h5 style="color: #ff9d01; font-family: Poppins; font-weight: bold;">Hi
+                           ${localStorage["username"]},</script> please
+                            signin
+                            to continue
+                        </h5>
+                       <form autocomplete="off">   
+                            <label for=""><i class="fas fa-unlock-alt"></i> Password</label>
+                            <div class="login-row row no-margin">
+                               
+                                <input id="password" type="password" autocomplete="new-password"
+                                    class="form-control form-control-sm">
+                                    <br>
+                                    <small id="togglePass" style="cursor:pointer; font-style:bold">Show password</small>
+                            </div>
+                        </form>    
+                        <br>
+                        <a  style="float: right; color: red;" href="./index.html">Log out</a>
 
 
                         <div class="login-row btnroo row no-margin">
-                          <button id="signin" onclick="reAuth()"
-                            class="btn btn-primary btn-sm ">Sign
-                            In</button>
+                            <button id="signin" onclick="reAuth()"
+                                class="btn btn-primary btn-sm ">Sign
+                                In</button>
                         </div>
 
                         <br />
 
                     </div>
                     <footer class="footer">
-                      <div style="display: flex;
-                        justify-content: center;" class="copyright">© <a style="color: #051f3e;"
-                          href="../#"><b>
-                            Dextroux Technologies</b></a></div>
+                        <div style="display: flex;
+                        justify-content: center;" class="copyright">© <a  style="color: #051f3e;"
+                                href="../#"><b>
+                                    Dextroux Technologies</b></a></div>
                     </footer>
-                  </div>
-
                 </div>
-                <script>
-                  const password = document.querySelector('#password');
-                  togglePass.addEventListener('click', function (e) {
+
+            </div>
+            <script>
+                const password = document.querySelector('#password');
+                togglePass.addEventListener('click', function (e) {
                     // toggle the type attribute
                     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-                  password.setAttribute('type', type);
-                  parent.document.getElementById('togglePass').innerHTML = parent.document.getElementById('togglePass').innerHTML == 'Show password' ? 'Hide password' : 'Show password';
+                    password.setAttribute('type', type);
+                    parent.document.getElementById('togglePass').innerHTML = parent.document.getElementById('togglePass').innerHTML == 'Show password' ? 'Hide password' : 'Show password';
                 })
-                </script>
-                <script src="../assets/js/lib/toastr/toastr.min.js"></script>
-                <script src="../assets/js/lib/toastr/toastr.init.js"></script>
-                <script src="../assets/js/lib/sweetalert/sweetalert.min.js"></script>
-                <script src="../assets/js/lib/sweetalert/sweetalert.init.js"></script>
-            </div>
-          </div>
+            </script>
+            <script src="../assets/js/lib/toastr/toastr.min.js"></script>
+            <script src="../assets/js/lib/toastr/toastr.init.js"></script>
+            <script src="../assets/js/lib/sweetalert/sweetalert.min.js"></script>
+            <script src="../assets/js/lib/sweetalert/sweetalert.init.js"></script>
         </div>
-      </div>
-      `;
+    </div>
+</div>
+</div>
+`;
 
   authenticationModal = parent.document.getElementById("authenticationModal");
   if (authenticationModal != null) {
     return 0;
   }
 
+  // JQUERY LIBRARY
+  //document.body.innerHTML += `<script src="../js/jquery-3.3.1.min.js"></script>`;
+
   parent.$("body").append(modal);
-  parent.$("#authenticationModal").modal({ backdrop: "static", keyboard: false });
+  parent
+    .$("#authenticationModal")
+    .modal({ backdrop: "static", keyboard: false });
   parent.$("#authenticationModal").modal("show");
 }
 
