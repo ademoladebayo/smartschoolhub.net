f611fe0ae83a83289cf2b044786f281925591108
ademola.adebayo <adeademola.adebayo@etranzact.com>
2025-07-15 19:01:32 +0100

    USE CBT AS  -- V1.5

--- portal/teacher/js/teacher.js
+++ portal/teacher/js/teacher.js
@@ -2990,7 +2990,7 @@ function getResultForCBT() {
 
       header = data.result_settings;
       for (key in Object.keys(header)) {
-        console.log("TESTING" + header[key]);
+        console.log("TESTING" + key);
         if (header[key] == 'sn' || header[key] == 'fullname' || header[key].status == 'active') {
           continue;
         }
