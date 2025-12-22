0a2dfaf5fb4cb6183a73b55a30821073d20fa502
ademola.adebayo <ademola.adebayo@etranzact.com>
2025-12-22 23:33:49 +0100

    DUPLICATE RESULT CHECK -- v1.8

--- portal/backend/app/Service/AdminService.php
+++ portal/backend/app/Service/AdminService.php
@@ -850,9 +850,9 @@ class AdminService
                 $wrong_class = $wrong_class->class_id;
             }
 
-            $correct_class_registration =  SubjectRegistrationModel::select("id")->where(["class_id" => $correct_class->id, "user_id" => $student->id, 'session' => $session, 'term' => $term])->get();
+            $correct_class_registration =  SubjectRegistrationModel::select("id")->where(["class_id" => $correct_class->id, "student_id" => $student->id, 'session' => $session, 'term' => $term])->get();
 
-            $wrong_class_registration = SubjectRegistrationModel::select("id")->where(["class_id" => $wrong_class, "user_id" => $student->id, 'session' => $session, 'term' => $term])->get();
+            $wrong_class_registration = SubjectRegistrationModel::select("id")->where(["class_id" => $wrong_class, "student_id" => $student->id, 'session' => $session, 'term' => $term])->get();
 
             $object =
                 [
